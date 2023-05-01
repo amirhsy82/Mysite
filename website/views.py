fdd7b8b6b5e896c60a42830f4d1e31fd58c5c6e0
amirhsy82 <125390977+amirhsy82@users.noreply.github.com>
2023-05-01 13:40:31 +0430

    General Template folder
    Created a general folder for all templates in Project

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 def about_view(request):
     return render(request, 'website/about.html')
 

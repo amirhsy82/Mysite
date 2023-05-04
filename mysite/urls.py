eec901d1549d7f0fe5e6311c0ca83aa68915fb61
amirhsy82 <125390977+amirhsy82@users.noreply.github.com>
2023-05-04 11:44:44 +0430

    added static DIR and URLs in setting
    static root
    static url
    media root
    media url

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # form "Priject's Directory"."FileName" import "function"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('website.urls'))
 ]
+
+# For Statics:
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# For Media:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

c8b9b70cbf6e3564febc1673b162c3faae30ee33
amirhsy82 <125390977+amirhsy82@users.noreply.github.com>
2023-05-16 14:02:07 +0430

    Completed Post and Contact Model
    Completed Post model in Blog App - Created Contact Model in website app and migrated all of them.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 class Post(models.Model):
+    # author
+    # image
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # category 
+    # tag 
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+
 

dcf973353bf30b0d61e2461392e20065beaf1aba
amirhsy82 <125390977+amirhsy82@users.noreply.github.com>
2023-05-22 12:56:24 +0330

    extracting data from database into blog-home template

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,6 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    fields = ('title',)
     list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
     list_filter = ('status',)
     ordering = ('created_date',)
--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 # form "Priject's Directory"."FileName" import "function"
-from blog.views import blog_view, blog_single
+from blog.views import *
+
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', blog_view, name='index'),
-    path('single', blog_single, name='single')
+    path('single', blog_single, name='single'),
+    path('test', test, name='test'),
 ]

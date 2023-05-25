7ce29a71da35a41b04e143e799ba0739c488bec7
amirhsy82 <125390977+amirhsy82@users.noreply.github.com>
2023-05-25 13:05:59 +0330

    author field in Post model created - blog-home template updated

--- blog/views.py
+++ blog/views.py
@@ -8,11 +8,12 @@ def blog_view(request):
     return render(request, 'blog/blog-home.html', context)
 
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    post = get_object_or_404(Post, id=pid, status=1)
+    context = {'post':post}
+    return render(request, 'blog/blog-single.html', context)
 
 def test(request, pid):
-    # post = Post.objects.get(id=pid)
     post = get_object_or_404(Post, id=pid)
     context = {'post':post}
     return render(request, 'test.html', context)

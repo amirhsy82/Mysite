81c09208a6aabadfe1d13724b5435b4efd77ce2b
amirhsy82 <125390977+amirhsy82@users.noreply.github.com>
2023-12-27 11:28:18 +0330

    blog author urls added - blog views has been optimized

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,12 @@ from blog.models import Post
 
 
 # Create your views here.
-def blog_view(request, cat_name=None):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
-    if cat_name != None:
-        posts = posts.filter(category__name=cat_name)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -17,12 +19,6 @@ def blog_single(request, pid):
     post = get_object_or_404(Post, id=pid, status=1)
     context = {'post':post, 'posts':posts}
     return render(request, 'blog/blog-single.html', context)
-
-def blog_category(request, cat_name):
-    posts = Post.objects.filter(status=1)
-    posts = posts.filter(category__name=cat_name)
-    context = {'posts':posts}
-    return render(request, 'blog/blog-home.html', context)
 
 def test(request):
     return render(request, 'test.html')

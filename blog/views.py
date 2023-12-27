c7b941933c0bb3fd953254d11237487de1e46ae7
amirhsy82 <125390977+amirhsy82@users.noreply.github.com>
2023-12-28 00:38:01 +0330

    paginator added - database updated

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 
 # Create your views here.
 def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
+
     if kwargs.get('cat_name') != None:
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username=kwargs['author_username'])
+
+    posts = Paginator(posts, 3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -26,7 +37,7 @@ def test(request):
 def blog_search(request):
     posts = Post.objects.filter(status=1)
     if request.method == 'GET':
-        if search := request.Get.get('search'):
+        if search := request.GET.get('search'):
             posts = posts.filter(content__contains=search)
     context = {'post':posts, 'posts':posts}
     return render(request, 'blog/blog-home.html', context)

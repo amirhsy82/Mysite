44c7e20fec9ad899e6a1b73693e5a6ec5a92cb9c
amirhsy82 <125390977+amirhsy82@users.noreply.github.com>
2024-01-07 11:30:59 +0330

    pop up notification added

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,11 +1,10 @@
 from django import template
 from blog.models import Post, Category
+from django.utils import timezone
 
 register = template.Library()
 
 @register.inclusion_tag('website/website-blog-post.html')
 def latestblog(arg=6):
-    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
-    categories = Category.objects.all()
-    post_dict = {}
+    posts = Post.objects.filter(status=1, published_date__lte=timezone.now()).order_by('published_date')[:arg]
     return {'posts':posts}
--- website/views.py
+++ website/views.py
@@ -12,12 +12,14 @@ def about_view(request):
 
 def contact_view(request):
     if request.method == 'POST':
+      
         form = ContactForm(request.POST)
         if form.is_valid():
+            form.instance.name = 'Unknown'
             form.save()
             messages.add_message(request, messages.SUCCESS, 'Your ticket submitted sucessfuly')
         else:
-             messages.add_message(request, messages.ERROR, "Your ticket didn't submit")
+            messages.add_message(request, messages.ERROR, "Your ticket didn't submit")
     form = ContactForm()
     return render(request, 'website/contact.html', {'form':form})
 
